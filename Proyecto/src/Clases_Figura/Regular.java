3df558462e0d3621bb1fefc88805bcefdaad31e7
Sebastian <Sebarem723@gmail.com>
2019-03-31 12:32:55 -0300

    Agregada la clase Regular al Packages Clases_Figura

---
+++ Proyecto/src/Clases_Figura/Regular.java
@@ -0,0 +1,22 @@
+/*
+Clase que se encarga de dibujar las figuras regulares 
+*/
+package Clases_Figura;
+
+import javafx.scene.canvas.Canvas;
+
+/**
+ *
+ * @author Sebastian
+ */
+public abstract class Regular {
+    
+    int x1,x2,x3,x4,y1,y2,y3,y4;
+    
+    
+    /*
+    Metodo que se encarga de dibujar la figura en primera instancia
+    */
+    public void dibujar(Canvas lienzo){}
+    
+}
