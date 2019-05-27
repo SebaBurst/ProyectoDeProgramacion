d4db5535873a77a95b9fdee5196f9838dd733e54
Kriss <>
2019-05-27 01:48:43 -0400

    Avance en decision

--- ProyectoV1/src/Clases_Figura/Decision.java
+++ ProyectoV1/src/Clases_Figura/Decision.java
@@ -15,55 +15,54 @@ import javafx.scene.text.FontWeight;
  *
  * @author Sebastian
  */
-public class Decision extends Figura{
-    
+public class Decision extends Figura {
 
     @Override
-    public void dibujar(GraphicsContext lienzo,int x,int y) {
+    public void dibujar(GraphicsContext lienzo, int x, int y) {
         lienzo.setStroke(Color.valueOf("#b44cd9"));
         this.setMedioX(x);
         this.setMedioY(y);
-        int ix= x;
-        int x1=x;
-        int y1=y;
-        int x2=x;
-        
-       
+        int ix = x;
+        int x1 = x;
+        int y1 = y;
+        int x2 = x;
+
         this.setX1(ix);
         this.setY1(y);
-        this.setX2(ix+190);
+        this.setX2(ix + 190);
         this.setY2(y);
-        this.setX3(ix-70);
-        this.setY3(y+70);
-        this.setX4(ix+120);
-        this.setY4(y+70);
-        
-        lienzo.setStroke(Color.valueOf("#b44cd9"));
-        lienzo.setLineWidth(3.0);
-        
-        for (int i = 0; i < 30; i++) {
-            lienzo.strokeLine(x,y+i, x+100, y+30);
-            lienzo.strokeLine(x,y+i, x-100,y+30);
-        }
-        int ys =y+70;
-        for (int i = 0; i < 40; i++) {
-            lienzo.strokeLine(x,ys-i, x+100, y+30);
-            lienzo.strokeLine(x-100,y+30, x, ys-i);
+        this.setX3(ix - 70);
+        this.setY3(y + 70);
+        this.setX4(ix + 120);
+        this.setY4(y + 70);
 
-
-        }
-        
-        System.out.println("ix: "+ix);
         lienzo.setStroke(Color.valueOf("#8a08b8"));
-        lienzo.strokeLine(x,y, x+100, y+30); //\
-        lienzo.strokeLine(x,y, x-100,y+30);//
-        lienzo.strokeLine(x-100,y+30, x, y+70);
-        lienzo.strokeLine(x,y+70, x+100, y+30);
+        lienzo.setLineWidth(3.0);
+
+        for (int i = 0; i < 30; i++) {
+            lienzo.strokeLine(x, y + i, x + 100, y + 30);
+            lienzo.strokeLine(x, y + i, x - 100, y + 30);
+        }
+        int ys = y + 70;
+        for (int i = 0; i < 40; i++) {
+            lienzo.strokeLine(x, ys - i, x + 100, y + 30);
+            lienzo.strokeLine(x - 100, y + 30, x, ys - i);
+
+        }
+
+        System.out.println("ix: " + ix);
+        lienzo.setStroke(Color.valueOf("#311740"));
+        lienzo.strokeLine(x, y, x + 100, y + 30); //\
+        lienzo.strokeLine(x, y, x - 100, y + 30);//
+        lienzo.strokeLine(x - 100, y + 30, x, y + 70);
+        lienzo.strokeLine(x, y + 70, x + 100, y + 30);
         lienzo.setStroke(Color.WHITE);
         lienzo.setFont(Font.font("Verdana", FontWeight.LIGHT, FontPosture.ITALIC, 15.0));
         lienzo.setFill(Color.BLACK);
-        lienzo.fillText(this.getTextoFigura(), Math.round(ix+25),Math.round(y+35));//y
+        lienzo.fillText(this.getTextoFigura(), Math.round(ix + 25), Math.round(y + 35));//y
+        proyectov1.ProyectoV1.dFlujo.dibujarDecision(x, y, y2, lienzo);
+
+
     }
 
 }
-
